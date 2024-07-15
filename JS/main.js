7090d87fc921f3a617267ea9ac585f5a4970d1fb
Guilherme Castro <gui.castro.0001.1426@gmail.com>
2024-07-15 20:15:50 -0300

    Salvando outra tela inicial

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,49 @@
+// document.querySelector('.intro-image').addEventListener('click', () => {
+//     document.querySelector('.intro-image').style.transform = 'scale(1.2)';
+//     document.querySelector('.intro-image').style.opacity = '0';
+//     setTimeout(() => {
+//       document.querySelector('.intro-image').style.display = 'none';
+//       document.querySelector('.site-content').classList.add('active');
+//     }, 1000); // 1 segundo para coincidir com a transição de 1s no CSS
+//   });
+
+//   const imgs = document.getElementById("img")
+// const img = document.querySelectorAll("#img img")
+
+// let idx = 0;
+
+// function Carrossel() {
+//     idx++;
+//     if (idx > img.length - 1) {
+//         idx = 0;
+//     }
+
+//     imgs.style.transform = `translateX(${-idx * 600}px)`
+// }
+// setInterval(Carrossel, 3000)
+// imgs.style.width = `${img.length * 600}px`
+
 document.querySelector('.intro-image').addEventListener('click', () => {
-    document.querySelector('.intro-image').style.transform = 'scale(1.2)';
-    document.querySelector('.intro-image').style.opacity = '0';
-    setTimeout(() => {
+  document.querySelector('.intro-image').style.transform = 'scale(1.2)';
+  document.querySelector('.intro-image').style.opacity = '0';
+  setTimeout(() => {
       document.querySelector('.intro-image').style.display = 'none';
       document.querySelector('.site-content').classList.add('active');
-    }, 1000); // 1 segundo para coincidir com a transição de 1s no CSS
-  });
+  }, 1000); // 1 segundo para coincidir com a transição de 1s no CSS
+});
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function Carrossel() {
+  idx++;
+  if (idx > img.length - 1) {
+      idx = 0;
+  }
+
+  imgs.style.transform = `translateX(${-idx * 300}px)`; /* Ajuste conforme necessário */
+}
+setInterval(Carrossel, 3000);
+imgs.style.width = `${img.length * 300}px`;
