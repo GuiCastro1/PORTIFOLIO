ef3cb1857799984155ae57514ccedde02affbee5
Guilherme Castro <gui.castro.0001.1426@gmail.com>
2024-07-21 18:35:43 -0300

    teste

--- JS/PORTI.js
+++ JS/PORTI.js
@@ -13,3 +13,12 @@ function Carrossel() {
 }
 setInterval(Carrossel, 3000)
 imgs.style.width = `${img.length *300}px`
+
+document.querySelector('.intro-image').addEventListener('click', () => {
+    document.querySelector('.intro-image').style.transform = 'scale(1.2)';
+    document.querySelector('.intro-image').style.opacity = '0';
+    setTimeout(() => {
+      document.querySelector('.intro-image').style.display = 'none';
+      document.querySelector('.site-content').classList.add('active');
+    }, 1000); // 1 segundo para coincidir com a transição de 1s no CSS
+  });
